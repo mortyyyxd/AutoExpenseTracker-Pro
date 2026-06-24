d142d024668ba8813e03124a45e0b08443c74a81
mortyyyxd <immorty64@gmail.com>
2026-06-24 18:58:08 +0300

    Add RepairManager class and RepairRecord struct

---
+++ include/RepairManager.h
@@ -0,0 +1,38 @@
+#ifndef REPAIR_MANAGER_H
+#define REPAIR_MANAGER_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct RepairRecord
+{
+    string date;
+    int mileage;
+    string category;
+    double cost;
+};
+
+class RepairManager
+{
+private:
+    vector<RepairRecord> repairs;
+
+public:
+    void addRepair();
+    void showRepairs() const;
+
+    void saveToFile(const string& filename) const;
+    void loadFromFile(const string& filename);
+
+    double getTotalRepairCost() const;
+
+    RepairRecord getMostExpensiveRepair() const;
+
+    vector<RepairRecord> getRecords() const;
+
+    int getRecordCount() const;
+};
+
+#endif
