11cede13bc465fe46b4032263ce520c0ddfeb596
mortyyyxd <immorty64@gmail.com>
2026-06-24 19:16:38 +0300

    Add new functions for editing and deleting repairs

--- include/RepairManager.h
+++ include/RepairManager.h
@@ -1,3 +1,4 @@
+```cpp
 #ifndef REPAIR_MANAGER_H
 #define REPAIR_MANAGER_H
 
@@ -20,12 +21,15 @@ private:
     vector<RepairRecord> repairs;
 
 public:
+    // Основні функції
     void addRepair();
     void showRepairs() const;
 
+    // Файли
     void saveToFile(const string& filename) const;
     void loadFromFile(const string& filename);
 
+    // Статистика
     double getTotalRepairCost() const;
 
     RepairRecord getMostExpensiveRepair() const;
@@ -33,17 +37,17 @@ public:
     vector<RepairRecord> getRecords() const;
 
     int getRecordCount() const;
+
+    // Нові функції
+    void editRepair();
+    void deleteRepair();
+
+    void searchByDate(const string& date) const;
+    void searchByCategory(const string& category) const;
+
+    void sortByMileage();
+    void sortByCost();
 };
 
-void editFuel();
-void deleteFuel();
-
-void searchByDate(const string& date) const;
-
-void sortByMileage();
-void sortByCost();
-
-FuelRecord getMostExpensiveFuel() const;
-FuelRecord getCheapestFuel() const;
-
 #endif
+```
